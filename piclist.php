0b7b8f15fa311f26feefe86bba2f2abeb7dc316e
sbholvan-blk <58177890+sbholvan-blk@users.noreply.github.com>
2020-02-09 18:32:02 +0530

    Update piclist.php

--- piclist.php
+++ piclist.php
@@ -2,9 +2,7 @@
 $dirname = "/var/www/html/MumbaiTechies/";
 $images = glob($dirname."*.jpeg");
 foreach($images as $image) {
-echo $image;
-    echo '<img src="'.$image.'" /><br/>';
+    echo '<h3 text-align="center">'.$image.'</h3><br/>';
+    echo '<center><img src="'.basename($image).'" /></center><br/>';
 }
-echo '<img sec="nature.jpeg" />';
-echo 'testv1';
 ?>
